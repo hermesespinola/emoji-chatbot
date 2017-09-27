a9c9616bcbf9cfd6a18b76645080fe737df9c513
Hermes Espínola <hermesespinola@gmail.com>
2017-09-27 13:19:32 -0500

    get tweets

--- get_tweets.py
+++ get_tweets.py
@@ -2,6 +2,7 @@
 import argparse
 import tweepy
 import csv
+import os
 
 consumer_key = '32qgMUGN2OqjQpB0JSeroN7Jv'
 consumer_secret = 'PQF5DGBWSIzM0MmYuOZQbv4ylkNaiAPqDa3qnhd4ZkSztUaR9g'
@@ -11,9 +12,14 @@ access_token_secret = 'nYoQx0LkScqdAtFCkKohkCleghtn18ibU0QWjoduy0OU9'
 parser = argparse.ArgumentParser(description='Extract tweets from user')
 parser.add_argument('--username', '-u', default='TheTweetOfGod',
                     help='username of tweet account to extract its tweets')
+parser.add_argument('--max_id', '-m', default=None,
+                    help='get tweets below maximum twitter id')
+parser.add_argument('--append', '-a', action='store_true',
+                    help='get tweets below maximum twitter id')
 
 args = parser.parse_args()
 screen_name = args.username
+max_id = args.max_id
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -21,7 +27,12 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 all_tweets = []
 
-new_tweets = api.user_timeline(screen_name=screen_name, count=200)
+print (max_id)
+new_tweets = []
+if max_id:
+    new_tweets = api.user_timeline(screen_name=screen_name, count=200)
+else:
+    new_tweets = api.user_timeline(screen_name=screen_name, count=200, max_id=int(max_id))
 all_tweets.extend(new_tweets)
 
 oldest = all_tweets[-1].id - 1
@@ -35,7 +46,11 @@ while len(new_tweets) > 0:
 
 out_tweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in all_tweets]
 
-with open('%s_tweets.csv' % screen_name, 'w') as f:
+dialect = 'w'
+if args.append:
+    dialect = 'a'
+with open('%s_tweets.csv' % screen_name, dialect) as f:
     writer = csv.writer(f)
-    writer.writerow(['id','created_at','text'])
+    if dialect == 'w':
+        writer.writerow(['id','created_at','text'])
     writer.writerows(out_tweets)
